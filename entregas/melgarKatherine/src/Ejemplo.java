77d3d493d33e7d55e5256ff4085f777e73a29866
Katherine Ariana Melgar Echegoyen <katherine.melgar@unealum.local>
2026-02-10 13:28:31 +0100

    Estructura de funciones inicial, variables definidas

--- entregas/melgarKatherine/src/Ejemplo.java
+++ entregas/melgarKatherine/src/Ejemplo.java
@@ -2,6 +2,33 @@ import java.util.Scanner;
 
 public class Ejemplo{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] textoAModificar = new String[10];
+        llenarVariableConOpciones(textoAModificar);
+        preguntarAlUsuarioOpciones(textoAModificar, sc);
+        mostrarMenuAlUsuario(textoAModificar);
+    }
+
+    static void llenarVariableConOpciones(String[] mostrarMenuAlUsuario){
+        mostrarMenuAlUsuario[0] = "0: | Bienvenidos al editor EDLIN";
+        mostrarMenuAlUsuario[1] = "1:*| Utilice el menu inferior para editar el texto";
+        mostrarMenuAlUsuario[2] = "2: | ------";
+        mostrarMenuAlUsuario[3] = "3: | [L] permite definir la linea activa";
+        mostrarMenuAlUsuario[4] = "4: | [E] permite editar la linea activa";
+        mostrarMenuAlUsuario[5] = "5: | [I] permite intercambiar dos lineas";
+        mostrarMenuAlUsuario[6] = "6: | [B] borra el contenido de la linea activa";
+        mostrarMenuAlUsuario[7] = "7: | [D] deshace la última acción realizada";
+        mostrarMenuAlUsuario[8] = "8: | [S] sale del programa";
+        mostrarMenuAlUsuario[9] = "9: | ";
+    }
+
+    static void preguntarAlUsuarioOpciones(String[] textoAModificar, Scanner sc){
         
+    }
+
+    static void mostrarMenuAlUsuario(String[] mostrarMenuAlUsuario){
+        for(int i = 0; i < mostrarMenuAlUsuario.length; i++){
+            System.out.println(mostrarMenuAlUsuario[i]);
+        }
     }
 }
