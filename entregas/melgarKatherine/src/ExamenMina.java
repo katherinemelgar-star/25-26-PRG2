c701caacb53736d8432ef529c3e3faead5f9dbb6
Katherine Ariana Melgar Echegoyen <katherine.melgar@unealum.local>
2026-02-17 13:37:34 +0100

    Cambio de nombres de constantes, cambio de matrices manuales

--- entregas/melgarKatherine/src/ExamenMina.java
+++ entregas/melgarKatherine/src/ExamenMina.java
@@ -1,106 +1,82 @@
 import java.util.Scanner;
 
 public class ExamenMina{
+	static final int FILAS = 5;
+    static final int COLUMNAS = 7;
+    static final int TOTAL_MINAS = 5;
+    static final int MAX_EXPLOSIONES = 3;
 
-	static String[][] mapaMinasMostrar = 	
-		{ //[0][0]	[0][1]	[0][2]	[0][3]	[0][4]	[0][5]	[0][6]	[0][7]
-			{" ",	"1", 	"2", 	"3", 	"4",	"5",	"6", 	"7"},
-		  //[1][0]	[1][1]	[1][2]	[1][3]	[1][4]	[1][5]	[1][6]	[1][7]
-			{"1", 	"-", 	"-", 	"-", 	"-", 	"-", 	"-",	"-"},
-		  //[2][0]	[2][1]	[2][2]	[2][3]	[2][4]	[2][5]	[2][6]	[2][7]
-			{"2", 	"-", 	"-",	"-", 	"-", 	"-", 	"-",	"-"},
-		  //[3][0]	[3][1]	[3][2]	[3][3]	[3][4]	[3][5]	[3][6]	[3][7]
-			{"3", 	"-", 	"-", 	"-",	"-", 	"-", 	"-",	"-"},
-		  //[4][0]	[4][1]	[4][2]	[4][3]	[4][4]	[4][5]	[4][6]	[4][7]
-			{"4", 	"-", 	"-",	"-", 	"-", 	"-", 	"-",	"-"},
-		  //[5][0]	[5][1]	[5][2]	[5][3]	[5][4]	[5][5]	[5][6]	[5][7]
-			{"5", 	"-", 	"-",	"-", 	"-", 	"-", 	"-",	"-"}
-		};
-		
-	static String[][] mapaMinasActivas = 	
-		{ //[0][0]	[0][1]	[0][2]	[0][3]	[0][4]	[0][5]	[0][6]	[0][7]
-			{" ",	"1", 	"2", 	"3", 	"4",	"5",	"6", 	"7"},
-		  //[1][0]	[1][1]	[1][2]	[1][3]	[1][4]	[1][5]	[1][6]	[1][7]
-			{"1", 	"0", 	"0", 	"0", 	"0", 	"0", 	"0",	"0"},
-		  //[2][0]	[2][1]	[2][2]	[2][3]	[2][4]	[2][5]	[2][6]	[2][7]
-			{"2", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"},
-		  //[3][0]	[3][1]	[3][2]	[3][3]	[3][4]	[3][5]	[3][6]	[3][7]
-			{"3", 	"0", 	"0", 	"0",	"0", 	"0", 	"0",	"0"},
-		  //[4][0]	[4][1]	[4][2]	[4][3]	[4][4]	[4][5]	[4][6]	[4][7]
-			{"4", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"},
-		  //[5][0]	[5][1]	[5][2]	[5][3]	[5][4]	[5][5]	[5][6]	[5][7]
-			{"5", 	"0", 	"0",	"0", 	"0", 	"0", 	"0",	"0"}
-		};
+	static String[][] mapaMinasMostrar; 
+    static String[][] mapaMinasActivas;
 
-	//Posiciones Random de la mina
 	public static void main(String[] args){	
 	Scanner scan = new Scanner(System.in);
-		///////////////////////////
-		int i=0;	
-		while ( i < 5){
-			int PosYMina = (int)(Math.random()*2+1);
-			int PosXMina = (int)(Math.random()*4+1);
-			if (mapaMinasActivas[PosXMina][PosYMina]=="1"){
-			i=i-1;
-			}
-			else {
-				mapaMinasActivas[PosXMina+1][PosYMina+1]="1";
-			}
-			i++;
-		}
-	
-		int posX=0, posY=0, juegoEncendido=1, valoresCorrectos, contadorMapa=0, contadorMinas=0;
-		/////////////Comienza el juego
-		while(juegoEncendido==1){
-			for( i = 0; i<mapaMinasMostrar.length; i++){            
-				for (int j = 0; j<=mapaMinasMostrar.length+1; j++) {                                     
-					System.out.print(mapaMinasMostrar[i][j]);
-				}
-				System.out.println(" ");
-			}
-			valoresCorrectos=0;//reseteo de la variable
-			while(valoresCorrectos==0){
-				System.out.println(" ");
-				System.out.println("Ingrese X");
-				posY = scan.nextInt();
-				System.out.println("Ingrese Y");
-				posX = scan.nextInt();
-				if(posX>5){
-					valoresCorrectos=0;
-				}
-				else if(posY>7){
-					valoresCorrectos=0;
-				}
-				else{
-					valoresCorrectos=1;
-				}
-			}
+	mapaMinasMostrar = inicializarMapa("-");
+    mapaMinasActivas = inicializarMapa("0");
 
-			if(mapaMinasActivas[posX][posY]=="1"){
-			mapaMinasMostrar[posX][posY]="x";
-			contadorMinas++;
+	int i=0;	
+	while ( i < 5){
+		int PosYMina = (int)(Math.random()*2+1);
+		int PosXMina = (int)(Math.random()*4+1);
+		
+		if (mapaMinasActivas[PosXMina][PosYMina]=="1"){
+		i=i-1;
+		}
+		else {
+			mapaMinasActivas[PosXMina+1][PosYMina+1]="1";
+		}
+		i++;
+	}
+
+	int posX=0, posY=0, juegoEncendido=1, valoresCorrectos, contadorMapa=0, contadorMinas=0;
+	while(juegoEncendido==1){
+	for( i = 0; i<mapaMinasMostrar.length; i++){            
+		for (int j = 0; j<=mapaMinasMostrar.length+1; j++) {                                     
+			System.out.print(mapaMinasMostrar[i][j]);
 			}
-			else if(mapaMinasActivas[posX][posY]=="0"){
-			mapaMinasMostrar[posX][posY]=".";
+			System.out.println(" ");
+		}
+		valoresCorrectos=0;
+		while(valoresCorrectos==0){
+			System.out.println("Ingrese X");
+			posY = scan.nextInt();
+			System.out.println("Ingrese Y");
+			posX = scan.nextInt();
+			if(posX > FILAS || posY > COLUMNAS){
+				valoresCorrectos=0;
+				System.out.println("Coordenadas fuera de rango.");
 			}
 			else{
-			System.out.println("No es correcta esa opcion.");
+				valoresCorrectos=1;
 			}
-			
-			contadorMapa++;
-			if(contadorMinas>2){
-				juegoEncendido=0;
-				System.out.println("Has perdido");
-			}
-			else if(contadorMapa>=31){
-				System.out.println("Felicidades Ganador!");
-				juegoEncendido=0;
-			}
-			else{
-				
-			}
+		}
+		if(mapaMinasActivas[posX][posY]=="1"){
+		mapaMinasMostrar[posX][posY]="*";
+		contadorMinas++;
+		}
+		else{
+		mapaMinasMostrar[posX][posY]=".";
+		}
+		
+		contadorMapa++;
+		if(contadorMinas >= MAX_EXPLOSIONES){
+			juegoEncendido=0;
+			System.out.println("Has perdido: 3 explosiones");
+		}
+		else if(contadorMapa >= 31){
+			System.out.println("Felicidades Ganador!");
+			juegoEncendido=0;
+		}
 		}
 	}	
 		
-		
+	static String[][] inicializarMapa(String simbolo) {
+        String[][] matriz = new String[FILAS + 1][COLUMNAS + 1];
+        for (int i = 1; i <= FILAS; i++) {
+            for (int j = 1; j <= COLUMNAS; j++) {
+                matriz[i][j] = simbolo;
+            }
+        }
+        return matriz;
+    }	
 }
