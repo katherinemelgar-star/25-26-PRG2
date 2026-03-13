40399a69079fd9e03845690b2f9cfc0c613c44ac
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-03-13 09:36:24 +0000

    feat: agregar clase Intervalo con vista publica

---
+++ entregas/melgarKatherine/src/Intervalo.java
@@ -0,0 +1,80 @@
+class Intervalo{
+     private double puntoMedio;
+     private double longitud;
+
+     public Intervalo (double inferior, double superior) {
+
+    }
+
+    public Intervalo (double superior) {
+
+    }
+
+    public Intervalo () {
+
+    }
+    
+    public Intervalo (Intervalo intervalo) {
+
+    }
+
+    public Intervalo clone() {
+
+    }
+
+    public double longitud() {
+
+    }
+
+    public void desplazar(double desplazamiento) {
+
+    }
+
+    public Intervalo desplazado(double desplazamiento) {
+
+    }
+
+    public boolean incluye(double punto) {
+
+    }
+
+    public boolean incluye(Intervalo intervalo) {
+
+    }
+
+    public boolean equals(Intervalo intervalo) {
+
+    }
+
+    public Intervalo interseccion(Intervalo intervalo) {
+
+    }
+    
+    public boolean intersecta(Intervalo intervalo) {
+
+    }
+    
+    public void oponer() {
+ 
+    }
+
+    public void doblar() {
+ 
+    }
+
+
+    public void mostrar() {
+
+    }
+
+    public Intervalo[] trocear(int numeroTrozos) {
+
+
+    }
+    
+    public double puntoMedio() {
+
+    };
+
+
+}
