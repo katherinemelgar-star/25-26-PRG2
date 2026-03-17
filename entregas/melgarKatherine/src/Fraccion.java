9ad5f4e7cc07eebee9daef932a62f2cee837970d
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-03-17 12:48:01 +0000

    feat: constructores

--- entregas/melgarKatherine/src/Fraccion.java
+++ entregas/melgarKatherine/src/Fraccion.java
@@ -1,4 +1,26 @@
 public class Fraccion{
 	private int numerador;
     private int denominador;
+
+    public Fraccion(int numerador, int denominador) {
+        if (denominador == 0) {
+            throw new IllegalArgumentException("El cero no puede ser el denominador");
+        }
+        this.numerador = numerador;
+        this.denominador = denominador;
+        this.simplificar();
+    }
+
+    public Fraccion(int numero) {
+        this(numero, 1);
+    }
+
+    public Fraccion(Fraccion fraccion) {
+        this(fraccion.numerador, fraccion.denominador);
+    }
+
+    public Fraccion() {
+        this(0, 1);
+    }
+
 }
