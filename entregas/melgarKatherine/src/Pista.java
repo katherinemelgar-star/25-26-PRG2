6bb12a64db918a54cf632ffe1270801963671abf
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-04-27 13:45:56 +0000

    feat: implementar método mostrar en la clase Pista para visualizar caballos

--- entregas/melgarKatherine/src/Pista.java
+++ entregas/melgarKatherine/src/Pista.java
@@ -2,9 +2,17 @@ class Pista {
     final private int LONGITUD = 40;
     private Console console;
 
-    public Pista(){}
+    public Pista(){
+        console = new Console();
+    }
 
-    public void mostrar(){}
+    public void mostrar(Caballo[] caballos){
+        this.imprimirBorde();
+        this.imprimirCarril(caballos[0]);
+        this.imprimirCarril(caballos[1]);
+        this.imprimirBorde();
+        console.writeln();
+    }
 
     private void imprimirBorde(){}
 
