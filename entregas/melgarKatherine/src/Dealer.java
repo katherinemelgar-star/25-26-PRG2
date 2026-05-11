24ae63339bc82f74e9ca5be25ed2c8c1ae7c2310
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-05-11 13:30:18 +0000

    feat: implementar el método jugar en la clase Dealer y agregar lógica para manejar el juego

--- entregas/melgarKatherine/src/Dealer.java
+++ entregas/melgarKatherine/src/Dealer.java
@@ -3,13 +3,34 @@ public class Dealer {
     private Mano mano;
     private Console console;
 
-    public Dealer() {}
+    public Dealer() {
+        console = new Console();
+        menu = new Menu();
+    }
 
-    public void jugar() {}
+    public void jugar() {
+        boolean jugando = true;
+        this.reiniciar();
 
-    private void reiniciar() {}
+        do {
+            mano.mostrar();
+            menu.mostrar();
+            
+            switch (menu.pedirOpcion()) {
+                case 1 -> this.pedir();
+                case 2 -> this.reiniciar();
+                case 3 -> jugando = false;
+            }
+        } while (jugando);
+    }
 
-    private void pedir() {}
+    private void reiniciar() {
+    }
 
-    public static void main(String[] args) {}
+    private void pedir() {
+    }
+
+    public static void main(String[] args) {
+        new Dealer().jugar();
+    }
 }
