5cc2e34279bf7227a5b2fd4a04cc5a340bd5b065
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-05-11 13:33:54 +0000

    feat: implementar métodos para recibir cartas y gestionar la mano en la clase Mano

--- entregas/melgarKatherine/src/Mano.java
+++ entregas/melgarKatherine/src/Mano.java
@@ -6,11 +6,22 @@ public class Mano {
     final private int PUNTAJE_GANADOR = 21;
     final private int MAXIMO_CARTAS = 11;
 
-    public Mano() {}
+    public Mano() {
+        cartas = new Carta[MAXIMO_CARTAS];
+        ultima = 0;
+        console = new Console();
+    }
 
-    public void recibir() {}
+    public void recibir(Carta carta) {
+        carta.voltear();
+        this.poner(carta);
+    }
 
-    private void poner() {}
+    private void poner(Carta carta) {
+        assert ultima < MAXIMO_CARTAS : "Superó el limite de cartas en la mano";
+        cartas[ultima] = carta;
+        ultima++;
+    }
 
     public void mostrar() {}
 
