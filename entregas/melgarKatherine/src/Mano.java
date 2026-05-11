1eccf0601b95be05f6f8c0d9b4b518128e435723
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-05-11 13:25:30 +0000

    feat: implementar la clase Menu y definir métodos para mostrar opciones y pedir selección

--- entregas/melgarKatherine/src/Mano.java
+++ entregas/melgarKatherine/src/Mano.java
@@ -2,4 +2,24 @@ public class Mano {
     private Carta[] cartas;
     private int ultima;
     private Console console;
+
+    final private int PUNTAJE_GANADOR = 21;
+    final private int MAXIMO_CARTAS = 11;
+
+    public Mano() {}
+
+    public void recibir() {}
+
+    private void poner() {}
+
+    public void mostrar() {}
+
+    public boolean haGanado() {}
+
+    public boolean haPerdido() {}
+
+    public boolean puedeJugar() {}
+
+    private int obtenerPuntaje() {}
+
 }
