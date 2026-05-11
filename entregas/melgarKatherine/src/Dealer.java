48a68528b56f7d1aa6b4cc93b10c8aabf978412f
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-05-11 13:18:57 +0000

    feat: implementar la clase Dealer y agregar la clase Menu

--- entregas/melgarKatherine/src/Dealer.java
+++ entregas/melgarKatherine/src/Dealer.java
@@ -2,4 +2,14 @@ public class Dealer {
     private Baraja baraja;
     private Mano mano;
     private Console console;
+
+    public Dealer() {}
+
+    public void jugar() {}
+
+    private void reiniciar() {}
+
+    private void pedir() {}
+
+    public static void main(String[] args) {}
 }
